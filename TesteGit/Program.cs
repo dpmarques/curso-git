8ff4736a38577e6ea568c338d16bfaec1b27e2c0
Diego Marques <dih_markes@hotmail.com>
2021-01-25 20:11:25 -0300

    bom dia

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -3,7 +3,11 @@
 namespace TesteGit {
     class Program {
         static void Main(string[] args) {
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+
+            Console.ReadLine();
         }
     }
 }
